67a51473956876687c99ea92a400fbd4196927fa
iwata <iwata@7d18754b-3699-4886-a18b-2888e08888a8>
2010-02-08 03:19:46 +0000

    module-setupにて雛形を準備
    git-svn-id: https://svn.meganedev.net:4433/r/lang/perl/Hoya/trunk@340 7d18754b-3699-4886-a18b-2888e08888a8

---
+++ lib/Hoya.pm
@@ -0,0 +1,32 @@
+package Hoya;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Hoya -
+
+=head1 SYNOPSIS
+
+  use Hoya;
+
+=head1 DESCRIPTION
+
+Hoya is
+
+=head1 AUTHOR
+
+issm E<lt>issmxx@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
