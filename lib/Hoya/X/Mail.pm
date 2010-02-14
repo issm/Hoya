77cb18f0b2fdbbd0fec778bab869bddb9139fbc2
iwata <iwata@7d18754b-3699-4886-a18b-2888e08888a8>
2010-02-14 20:09:20 +0000

    MIME::Liteをラップしたメール送信用モジュール Hoya::X::Mail を追加
    git-svn-id: https://svn.meganedev.net:4433/r/lang/perl/Hoya/trunk@373 7d18754b-3699-4886-a18b-2888e08888a8

---
+++ lib/Hoya/X/Mail.pm
@@ -0,0 +1,224 @@
+package Hoya::X::Mail;
+use strict;
+use utf8;
+use base qw/Class::Accessor::Faster/;
+
+use MIME::Lite;
+use MIME::Types;
+use File::Basename;
+use Text::MicroTemplate::Extended;
+use Try::Tiny;
+use Carp;
+
+use Hoya::Util;
+
+my $_types;
+
+__PACKAGE__->mk_accessors(
+    qw/from to cc bcc subject
+       body signature _files
+       smtp_host
+       template_dir
+      /
+  );
+
+
+sub init {
+    my $self = shift;
+    $self->_files([]);
+
+    $_types = MIME::Types->new;
+
+    return $self;
+}
+
+
+sub body_from_template {
+    my ($self, $params) = @_;
+    my $var  = $params->{var} || {};
+    my $name = $params->{name};
+    my $body = '';
+
+    my $mt = Text::MicroTemplate::Extended->new(
+        include_path  => $self->template_dir || '.',
+        template_args => {
+            env  => \%ENV,
+            map {
+                ($_ => $var->{$_});
+            } keys %$var,
+            # ^ 各変数を，テンプレートのトップの名前空間に結びつける
+        },
+        use_cache => 1,
+    );
+
+    try {
+        $body = $mt->render(
+            name2path($name)
+        )->as_string;
+    }
+    catch {
+        my $class = ref $self;
+        carp shift;
+        carp "[$class\::body_from_template] template named '$name' not found.";
+    };
+
+    $self->body($body);
+    return $body;
+}
+
+
+sub attach {
+    my ($self, @files) = @_;
+
+    for my $f (@files) {
+        next  unless $self->_check_file($f);
+        push @{$self->_files}, $f;
+    }
+
+    return 1;
+}
+
+sub _check_file {
+    my ($self, $file) = @_;
+    my $class = ref $self;
+    my $okng = 0;
+
+    # ハッシュリファレンスでない
+    if (ref $file ne 'HASH') {
+        carp "[$class\::attach] not a hash reference.";
+        return 0;
+    }
+    # ->{path} が指定されていない
+    unless (is_def $file->{path}) {
+        carp "[$class\::attach] path is required.";
+        return 0;
+    }
+    # ->{path} が参照するファイルが存在しない
+    unless (-f $file->{path}) {
+        carp "[$class\::attach] file specified by 'path' not exists.";
+        return 0;
+    }
+
+    $file->{name} = basename($file->{path})
+        unless is_def $file->{name};
+
+    $file->{type} = '' . $_types->mimeTypeOf($file->{path})
+        unless is_def $file->{type};
+
+    return 1;
+}
+
+
+sub send {
+    my $self = shift;
+    my $debug = shift;
+
+    $self->body('')  unless is_def $self->body;
+
+    try {
+        my $class = ref $self;
+        die "[$class] to, from, subject, body and/or smtp_host is/are not defined."
+            unless is_def(
+                $self->to,
+                $self->from,
+                $self->subject,
+                $self->body,
+                $self->smtp_host,
+            );
+
+        $self->subject(de $self->subject);
+        $self->body(de $self->body);
+
+        my $mail = MIME::Lite->new(
+            From    => $self->from,
+            To      => $self->to,
+            Subject => $self->subject,
+            #Cc      => '',
+            #Bcc     => '',
+            Type    => 'multipart/mixed',
+        );
+
+        $mail->attach(
+            Type => 'TEXT',
+            Data => $self->body,
+        );
+
+        for my $f (@{$self->_files}) {
+            $mail->attach(
+                Path        => $f->{path},
+                Type        => $f->{type},
+                Filename    => $f->{name},
+                Desposition => 'attachment',
+            );
+        }
+
+        $mail->send(
+            'smtp',
+            $self->smtp_host,
+            Debug => $debug,
+        );
+    }
+    catch {
+        carp shift;
+    };
+}
+
+
+
+1;
+__END__
+
+=head1 NAME
+
+Hoya::X::Mail - A mail sender using MIME::Lite
+
+=head1 SYNOPSIS
+
+  use Hoya::X::Mail;
+
+=head1 DESCRIPTION
+
+  Hoya::X::Mail is
+
+=head1 METHODS
+
+=over 4
+
+=item new
+
+new
+
+=item init
+
+init
+
+=item body;
+
+=item body($message);
+
+gets/sets 'body'.
+
+=item body_from_template(\%params);
+
+message body using Text::MicroTemplate::Extended.
+
+=item attach(\%file1, \%file2, ...);
+
+=send send($debug);
+
+send
+
+=back
+
+=head1 AUTHOR
+
+issm E<lt>issmxx@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
