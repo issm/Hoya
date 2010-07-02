3656ae4b59377f5016923c7ceecb0ee575f13c7b
iwata <iwata@7d18754b-3699-4886-a18b-2888e08888a8>
2010-07-02 01:25:32 +0000

    Hoya::Plack::Middleware::Static::Skin を微修正．
     * 処理内容に変化はなし．
    
    git-svn-id: https://svn.meganedev.net:4433/r/lang/perl/Hoya/trunk@529 7d18754b-3699-4886-a18b-2888e08888a8

--- lib/Hoya/Plack/Middleware/Static/Skin.pm
+++ lib/Hoya/Plack/Middleware/Static/Skin.pm
@@ -22,7 +22,6 @@ sub call {
 
 sub _handle_static {
     my($self, $env) = @_;
-
     my $site = $self->site || $env->{HOYA_SITE};
     my $path_re = $self->path || Hoya::Re::PATH_STATIC_SKIN;
     my $path = do {
@@ -37,12 +36,12 @@ sub _handle_static {
     $static_root = "${project_root}/$static_root"  if $project_root;
     # v $env->{HOYA_SITE}における指定のファイルが存在しない場合，
     # v site/defaultにおける同名のファイルをリクエストする
-    #     → うまくいかん
-    unless (my @st = stat "${static_root}/$env->{PATH_INFO}") {
-        return  if $site ne 'default';
-        #$static_root = "site/default/$env->{HOYA_SKIN}";
-        #$static_root = "$env->{PROJECT_ROOT}/$static_root"
-        #    if $env->{PROJECT_ROOT};
+    # v   → うまくいかん
+    (my $file = "${static_root}/$env->{PATH_INFO}") =~ s{/+}{/}g;
+    unless (my @st = stat $file) {
+       return  if $site ne 'default';
+       #$static_root = "${project_root}/site/default/$env->{HOYA_SKIN}";
+       #($file = "${static_root}/$env->{PATH_INFO}") =~ s{/+}{/}g;
     }
 
     $self->{file} ||= Plack::App::File->new({
