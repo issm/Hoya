cb0591e5a850b7892ad1ff924df156da1767e849
iwata <iwata@7d18754b-3699-4886-a18b-2888e08888a8>
2010-07-02 01:25:25 +0000

    Hoya::DSH::DBIx::Skinny を修正．
     * DSN設定をDBクラスファイル（？）に直接書く仕様となったため，同モジュールで判別する処理をなくした．
    
    git-svn-id: https://svn.meganedev.net:4433/r/lang/perl/Hoya/trunk@524 7d18754b-3699-4886-a18b-2888e08888a8

--- lib/Hoya/DSH/DBIx/Skinny.pm
+++ lib/Hoya/DSH/DBIx/Skinny.pm
@@ -31,54 +31,39 @@ sub _init {
 sub _setup {
     my $self = shift;
     my $db_conf = $self->conf->{DSH}{$self->name};
-    my $db_type = $db_conf->{TYPE};
 
     my $skinny;
-    #
-    # mysql
-    #
-    if (lc $db_type eq 'mysql') {
-        try {
-            my $module_name;
-            if (
-                ($module_name) = $db_conf->{MODULE} =~ /^\+(.*)$/
-            ) {
-                1;
-            }
-            else {
-                $module_name = sprintf(
-                    '%s_DB_%s',
-                    $self->conf->{PROJECT_NAME},
-                    $db_conf->{MODULE},
-                );
-                $module_name = name2class $module_name;
-            }
-
-            eval "use ${module_name};";
-            $skinny = ${module_name}->new;
-
-            croak 'xxx'  unless defined $skinny;
+    try {
+        my $module_name;
+        if (
+            ($module_name) = $db_conf->{MODULE} =~ /^\+(.*)$/
+        ) {
+            1;
         }
-        catch {
-            my $msg = shift;
-            my $text = << "...";
+        else {
+            $module_name = sprintf(
+                '%s_DB_%s',
+                $self->conf->{PROJECT_NAME},
+                $db_conf->{MODULE},
+            );
+            $module_name = name2class $module_name;
+        }
+
+        eval "use ${module_name};";
+        $skinny = ${module_name}->new;
+
+        croak 'xxx'  unless defined $skinny;
+    }
+    catch {
+        my $msg = shift;
+        my $text = << "...";
 **** Error in Hoya::DSH::DBIx::Skinny ****
 
 $msg
 ...
-            croak $text;
-        };
-    }
-    #
-    # pgsql
-    #
-    elsif (lc $db_type eq 'pgsql') {
-    }
-    #
-    # sqlite
-    #
-    elsif (lc $db_type eq 'sqlite') {
-    }
+        croak $text;
+    };
+
 
     $self->_skinny($skinny);
     return $skinny;
