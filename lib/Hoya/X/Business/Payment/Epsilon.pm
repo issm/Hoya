127f88f395cc7c14b0c72997f4ba718edd80803e
issm <issmxx@gmail.com>
2011-01-03 13:57:27 +0900

    Hoya::X::Business::Payment::Epsilon: 仕様を拡張．
     * 決済処理を，イプシロンサイトへ遷移せずとも行えるよう，処理を修正した．（現状のサポート対象は，クレジットカード，コンビニのみ）
     * クレジットカードについては，スクレイピングを行って取得した情報を利用して決済処理を行っている．今後，対象ページが変更された場合には，対応する必要がある．
     * HTML::TreeBuilder，HTML::Entities を新たに利用するようにした．

--- lib/Hoya/X/Business/Payment/Epsilon.pm
+++ lib/Hoya/X/Business/Payment/Epsilon.pm
@@ -1,13 +1,17 @@
 package Hoya::X::Business::Payment::Epsilon;
 use strict;
 use warnings;
+use utf8;
 use parent qw/Hoya::X::Business::Payment/;
 use Encode;
 use Data::Recursive::Encode;
 use XML::Simple;
+use HTML::TreeBuilder;
+use HTML::Entities;
 use URI::Escape;
 use HTTP::Request;
 use HTTP::Request::Common;
+use Try::Tiny;
 use Hoya::Util;
 
 
@@ -18,14 +22,14 @@ __PACKAGE__->mk_accessors qw/contract_code passwd
 
 my $_conf = {
     url => {
-        submit_step1 => '',
+        submit_method => '',
         get_sales    => '',
     },
 };
 
 my $_conf_test = {
     url => {
-        submit_step1 => 'https://beta.epsilon.jp/cgi-bin/order/receive_order3.cgi',
+        submit_method => 'https://beta.epsilon.jp/cgi-bin/order/receive_order3.cgi',
         get_sales    => 'https://beta.epsilon.jp/client/getsales.cgi',
     },
 };
@@ -39,27 +43,35 @@ sub _init {
 
 
 
-sub submit_step1 {
+sub submit_method {
     my ($self, $param) = @_;
     $self->status(1);
+
+    my $type = $param->{type};
+    my $data = $param->{data};
 
     my $conf = $self->_conf;
 
     my $q = {
         contract_code => $self->contract_code,
+        process_code  => 1,
+        mission_code  => 1,
         xml           => 1,
     };
-    $q->{$_} = $param->{$_} for (
+
+    for my $k (
         qw/user_id user_name user_name_kana user_mail_add user_tel
            item_code item_name item_price
            order_number
            st_code mission_code process_code conveni_code
+           card_code card_number expire_m expire_y
            memo1 memo2
           /,
-    );
+    ) {
+        $q->{$k} = encode('sjis', $data->{$k})  if defined $data->{$k};
+    }
 
-    my $res = $self->ua->post($conf->{url}{submit_step1}, $q);
-
+    my $res = $self->ua->post($conf->{url}{submit_method}, $q);
     my $doc = $self->_parse_xml($res->content);
 
     my $result = {};
@@ -72,6 +84,77 @@ sub submit_step1 {
 
     return $self->result->{result};
 };
+
+
+sub submit_card_info {
+    my ($self, $param) = @_;
+    my $result = {};
+    my $target_url = $self->result->{redirect};
+
+    my ($res, $html);
+
+    $res = $self->ua->get($target_url);
+    $html = $res->content;
+    my $tree = HTML::TreeBuilder->new;
+    $tree->parse($html);
+    $tree->elementify;
+    my $e = $tree->look_down('name', 'trans_code');
+    my $tx_code = $e->attr('value');
+    $tree->delete;
+
+    my $q = {
+        trans_code  => $tx_code,
+        card_code   => $param->{card_code},
+        card_number => $param->{card_number},
+        expire_m    => $param->{expire_m},
+        expire_y    => $param->{expire_y},
+        auth        => '登 録 Confirm',
+        xml         => 1,
+    };
+    $q->{$_} = encode('sjis', $q->{$_})  for keys %$q;
+
+    $res = $self->ua->post($target_url, $q);
+    my $headers = $res->headers;
+
+    my $url_redirect = $headers->header('Location') || '';
+
+    # 決済完了
+    if ($url_redirect) {
+        $result = {
+            result   => 1,
+            redirect => $url_redirect,
+        };
+    }
+    # エラー
+    else {
+        $result = {
+            result      => 0,
+            error_code  => 1,
+            error_text  => '申し訳ございません。通信上のエラーが発生しました。',
+        };
+
+        $html = decode('cp932', $res->content);
+
+        $tree = HTML::TreeBuilder->new;
+        $tree->parse($html);
+
+        # 経験に基づく
+        my ($e_error) = $tree->look_down(
+            _tag  => 'font',
+            color => '#B20026',
+        );
+        if (defined $e_error) {
+            $result->{error_text} = decode_entities( $e_error->{_content}[0] );
+            ( $result->{error_code} ) = $result->{error_text} =~ /^(\d+):/;
+        }
+
+        $tree->delete;
+    }
+
+    $self->result( $result );
+    return $result;
+}
+
 
 
 
@@ -111,8 +194,17 @@ sub _parse_xml {
     my ($self, $xml) = @_;
     $xml = decode( 'cp932', uri_unescape($xml) );
     $xml =~ s/x-sjis-cp932/utf-8/;
+    $xml =~ s/\&/&amp;/g;
 
-    my $doc = XML::Simple::XMLin($xml, ForceArray => 1);
+    my $doc;
+    try {
+        $doc = XML::Simple::XMLin($xml, ForceArray => 1);
+    }
+    catch {
+        local $Hoya::Util::DUMP_PREFIX = "[31m";
+        warn Dc shift;
+    };
+
     return $doc;
 }
 
